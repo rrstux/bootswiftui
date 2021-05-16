0d54bc9f28983a9f3d48fce425b0c7483a0998bb
Robert Sandru <robertsandru96@gmail.com>
2021-05-16 18:43:27 +0300

    Initial Commit

---
+++ Shared/bootswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootswiftuiApp.swift
+//  Shared
+//
+//  Created by Robert Sandru on 16.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct bootswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
