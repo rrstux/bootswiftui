8b13196774228f73fc009d97f658baeba942d2f4
Robert Sandru <robertsandru96@gmail.com>
2021-05-21 13:43:06 +0300

    Carousel Component: showing slides, disabling controls conditionally, some kickass slides for demo

--- Shared/Components/Carousel.swift
+++ Shared/Components/Carousel.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct CarouselData: Identifiable {
     
     var id = UUID()
+    var isContentConstrainedToSafeZone = true
     
     var background: () -> AnyView
-    var midLayer: () -> AnyView
+    var midLayer: () -> AnyView = { Rectangle().opacity(0.4).eraseToAnyView() }
     var content: () -> AnyView
 }
 
@@ -20,7 +21,7 @@ struct Carousel<Content>: View, Component where Content : View {
     
     var componentTheme: Theme
     
-    @State private var activeSlide: Int = 0
+    @State private var activeSlide: Int = 1
     
     @Binding var carouselData: [CarouselData]
     
@@ -29,6 +30,31 @@ struct Carousel<Content>: View, Component where Content : View {
     
     @Binding var isRendered: Bool
     @Binding var isHidden: Bool
+    @Binding var isShowingIndicators: Bool
+    
+    // MARK: Safe Zone Calculator
+    
+    @State private var leftControlViewSize: CGSize = .zero
+    @State private var rightControlViewSize: CGSize = .zero
+    private let leftControlViewWidthPercent: CGFloat = 0.15
+    private let rightControlViewWidthPercent: CGFloat = 0.15
+    
+    private var safeZone: EdgeInsets {
+        .init(top: 0,
+              leading: leftControlViewSize.width,
+              bottom: isShowingIndicators ? 50 : 0,
+              trailing: rightControlViewSize.width)
+    }
+    
+    // MARK: Slide Scrolling
+    
+    private var isFirstSlide: Bool {
+        return activeSlide == 0
+    }
+    
+    private var isLastSlide: Bool {
+        return activeSlide == carouselData.count-1
+    }
     
     internal var isDismissable: Bool = false
     
@@ -37,12 +63,14 @@ struct Carousel<Content>: View, Component where Content : View {
         carouselData: Binding<[CarouselData]>,
         isRendered: Binding<Bool>,
         isHidden: Binding<Bool>,
+        isShowingIndicators: Binding<Bool>,
         @ViewBuilder leftControlView: () -> Content?,
         @ViewBuilder rightControlView: () -> Content?) {
         self.componentTheme = componentTheme
         self._carouselData = carouselData
         self._isRendered = isRendered
         self._isHidden = isHidden
+        self._isShowingIndicators = isShowingIndicators
         self.leftControlView = leftControlView()
         self.rightControlView = rightControlView()
     }
@@ -51,33 +79,30 @@ struct Carousel<Content>: View, Component where Content : View {
         componentTheme: Theme,
         carouselData: Binding<[CarouselData]>,
         isRendered: Binding<Bool>,
-        isHidden: Binding<Bool>) where Content == AnyView {
+        isHidden: Binding<Bool>,
+        isShowingIndicators: Binding<Bool>) where Content == AnyView {
         self.init(
             componentTheme: componentTheme,
             carouselData: carouselData,
             isRendered: isRendered,
-            isHidden: isHidden) {
+            isHidden: isHidden,
+            isShowingIndicators: isShowingIndicators) {
             AnyView(
                 Image(systemName: "chevron.left")
                     .font(.title)
-                    .padding(.leading, 20)
-                    .padding(.trailing, 40)
                     .frame(maxHeight: .infinity)
                     .foregroundColor(componentTheme.config.text))
         } rightControlView: {
             AnyView(
                 Image(systemName: "chevron.right")
                     .font(.title)
-                    .padding(.leading, 40)
-                    .padding(.trailing, 20)
                     .frame(maxHeight: .infinity)
                     .foregroundColor(componentTheme.config.text))
         }
     }
     
     var body: some View {
-        VStack {
-            Text("lol")
+        GeometryReader { reader in
             ZStack {
                 TabView(selection: $activeSlide) {
                     ForEach(carouselData.indices) { i in
@@ -86,7 +111,9 @@ struct Carousel<Content>: View, Component where Content : View {
                                 .foregroundColor(.clear)
                                 .overlay(carouselData[i].background())
                             carouselData[i].midLayer()
-                            carouselData[i].content()
+                            carouselData[i]
+                                .content()
+                                .padding(carouselData[i].isContentConstrainedToSafeZone ? safeZone : .init())
                         }
                         .clipped()
                         .id(i)
@@ -94,87 +121,138 @@ struct Carousel<Content>: View, Component where Content : View {
                 }
                 carouselControls()
             }
-            .frame(height: 400)
-            .padding()
-            .tabViewStyle(PageTabViewStyle())
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: isShowingIndicators ? .always : .never))
     }
     
     fileprivate func carouselControls() -> some View {
-        return HStack {
-            Button(action: {
-                withAnimation {
-                    activeSlide -= 1
-                }
-            }, label: {
-                leftControlView
-            })
-            
-            Spacer()
-            Button(action: {
-                withAnimation {
-                    activeSlide += 1
-                }
-            }, label: {
-                rightControlView
-            })
+        return GeometryReader { reader in
+            HStack {
+                Button(action: {
+                    withAnimation {
+                        decreaseActiveSlide()
+                    }
+                }, label: {
+                    leftControlView
+                        .frame(width: reader.size.width * leftControlViewWidthPercent)
+                        .opacity(isFirstSlide ? 0 : 1)
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation {
+                        increaseActiveSlide()
+                    }
+                }, label: {
+                    rightControlView
+                        .frame(width: reader.size.width * rightControlViewWidthPercent)
+                        .opacity(isLastSlide ? 0 : 1)
+                })
+            }
+            .onAppear {
+                leftControlViewSize = CGSize(width: reader.size.width*leftControlViewWidthPercent, height: reader.size.height)
+                rightControlViewSize = CGSize(width: reader.size.width*rightControlViewWidthPercent, height: reader.size.height)
+            }
         }
+    }
+    
+    fileprivate func decreaseActiveSlide() {
+        if isFirstSlide { return }
+        activeSlide -= 1
+    }
+    
+    fileprivate func increaseActiveSlide() {
+        if isLastSlide { return }
+        activeSlide += 1
     }
 }
 
 struct Carousel_Previews: PreviewProvider {
     
     @State static var carouselData = [
-        CarouselData(background: {
-            Rectangle()
-                .foregroundColor(.clear)
-                .overlay(            Image(systemName: "swift")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .foregroundColor(.white))
-                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]),
-                                           startPoint: .leading,
-                                           endPoint: .bottom))
-                //                .background(Color.red)
-                .eraseToAnyView()
-        },
-        midLayer: {
-            Rectangle()
-                .opacity(0.4)
-                .eraseToAnyView()
-        },
-        content: {
-            HStack {
-                VStack {
-                    Text("First Slide Text")
-                        .foregroundColor(.white)
-                    Spacer()
+        
+        CarouselData(
+            isContentConstrainedToSafeZone: true,
+            background: {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(Image("LugojPasajBoldea"))
+                    .eraseToAnyView()
+            },
+            content: {
+                ZStack {
+                    VStack {
+                        Image(systemName: "building")
+                            .font(.system(size: 92))
+                            .foregroundColor(.white)
+                        Text("Lugoj")
+                            .font(.system(size: 62, weight: .heavy, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top)
+                        Text("Book tickets today. Arrive today and have fun!")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        
+                        Button(action: {}, label: {
+                            Text("Book tickets")
+                                .foregroundColor(Color.white)
+                                .font(.headline)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.green.clipShape(Capsule()))
+                        })
+                    }
                 }
-                Spacer()
-            }
-            .eraseToAnyView()
-        }),
-        CarouselData(background: {
-            Rectangle()
                 .eraseToAnyView()
-        },
-        midLayer: {
-            Text("Lol")
+            }),
+        CarouselData(
+            isContentConstrainedToSafeZone: true,
+            background: {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.orange]),
+                                               startPoint: .leading,
+                                               endPoint: .trailing))
+                    .eraseToAnyView()
+            },
+            midLayer: { Rectangle().foregroundColor(.clear).eraseToAnyView() },
+            content: {
+                ZStack {
+                    VStack {
+                        Image(systemName: "swift")
+                            .font(.system(size: 92))
+                            .foregroundColor(.white)
+                        Text("Swift.")
+                            .font(.system(size: 62, weight: .heavy, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top)
+                        Text("More performance. More stability. More open source.")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                        
+                        Button(action: {}, label: {
+                            Text("Try today")
+                                .foregroundColor(Color.white)
+                                .font(.headline)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue.clipShape(Capsule()))
+                        })
+                    }
+                }
                 .eraseToAnyView()
-        },
-        content: {
-            HStack {
-                Text("Second slide text")
-                Text("much wow!")
-            }
-            .foregroundColor(.white)
-            .eraseToAnyView()
-        })
+            })
     ]
     static var previews: some View {
-        Carousel(componentTheme: .primary,
+        Carousel(componentTheme: .warning,
                  carouselData: $carouselData,
                  isRendered: .constant(true),
-                 isHidden: .constant(false))
+                 isHidden: .constant(false),
+                 isShowingIndicators: .constant(true))
+            .frame(height: 800)
     }
 }
